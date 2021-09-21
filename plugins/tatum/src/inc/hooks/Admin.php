cab450a700c04cf1234a0f403fb4dd18a97409f1
Hathoriel <lukas.kotol@tatum.io>
2021-09-21 09:26:12 +0200

    Added temporary woocommerce product tab tatum icon

--- plugins/tatum/src/inc/hooks/Admin.php
+++ plugins/tatum/src/inc/hooks/Admin.php
@@ -17,9 +17,9 @@ class Admin
     public function add_product_data_icon() {
         ?>
       <style>
-          #woocommerce-product-data .pricing_options.active:hover > a:before,
-          #woocommerce-product-data .pricing_options > a:before {
-              background: url(<?php echo plugin_dir_url( 'public/assets/IMG_20210729_162902.jpg' ); ?>) center center no-repeat;
+          #woocommerce-product-data .tatum_options.active:hover > a:before,
+          #woocommerce-product-data .tatum_options > a:before {
+              background: url(<?php echo plugin_dir_url( 'tatum/public/assets/tatum.png' ). 'tatum.png'; ?>) center center no-repeat;
               content: " " !important;
               background-size: 100%;
               width: 13px;
@@ -29,15 +29,15 @@ class Admin
           }
 
           @media only screen and (max-width: 900px) {
-              #woocommerce-product-data .pricing_options.active:hover > a:before,
-              #woocommerce-product-data .pricing_options > a:before,
-              #woocommerce-product-data .pricing_options:hover a:before {
+              #woocommerce-product-data .tatum_options.active:hover > a:before,
+              #woocommerce-product-data .tatum_options > a:before,
+              #woocommerce-product-data .tatum_options:hover a:before {
                   background-size: 35%;
               }
           }
 
-          .pricing_options:hover a:before {
-              background: url(<?php echo plugin_dir_url( 'public/assets/IMG_20210729_162902.jpg' ); ?>) center center no-repeat;
+          .tatum_options:hover a:before {
+              background: url(<?php echo plugin_dir_url( 'tatum/public/assets/tatum.png' ). 'tatum.png'; ?>) center center no-repeat;
           }
 
       </style><?php
