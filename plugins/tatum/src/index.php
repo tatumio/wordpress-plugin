463ea819e5af8af565c9add0eca3b9e94efc79fe
Hathoriel <lukas.kotol@tatum.io>
2021-11-14 21:11:37 +0100

    Fixed safari date error.

--- plugins/tatum/src/index.php
+++ plugins/tatum/src/index.php
@@ -8,7 +8,7 @@
  * Description: 	NFT Maker is the easiest and fastest free plugin to work with NFTs without any blockchain development experience.
  * Author:          Tatum
  * Author URI:		https://tatum.io
- * Version: 		2.0.10
+ * Version: 		2.0.11
  * Text Domain:		tatum
  * Domain Path:		/languages
  * License:         MIT
