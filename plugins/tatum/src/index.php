157a4caa4692b6a5399ce4b0f9aee5730e7390a9
Hathoriel <lukas.kotol@tatum.io>
2022-01-03 11:13:21 +0100

    Tested with WordPress 5.8.2

--- plugins/tatum/src/index.php
+++ plugins/tatum/src/index.php
@@ -8,7 +8,7 @@
  * Description: 	NFT Maker is the easiest and fastest free plugin to work with NFTs without any blockchain development experience.
  * Author:          Tatum
  * Author URI:		https://tatum.io
- * Version: 		2.0.14
+ * Version: 		2.0.15
  * Text Domain:		tatum
  * Domain Path:		/languages
  * License:         MIT
